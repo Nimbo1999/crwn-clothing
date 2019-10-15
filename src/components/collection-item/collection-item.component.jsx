a74f7549f658e91198636d11e763e4d9a291337b
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-10-15 00:36:29 -0300

    att collection-preview e adicionado a collection-item

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './collection-item.styles.scss'
+
+const CollectionItem = ( {name, price, imageUrl} ) => (
+    <div className="collection-item">
+
+        <div className="image" style={{
+            backgroundImage: `url(${imageUrl})`
+        }} />
+
+        <div className="collection-footer">
+            <div className="name">{name}</div>
+            <div className="price">R${price},00</div>
+        </div>
+
+    </div>
+)
+
+export default CollectionItem;
