87f8729de4b58f8bd1c8449331672bdfa8c39e7d
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-11-12 13:08:33 -0300

    incluindo autenticação com email e senha e salvando no firebase.

--- src/App.js
+++ src/App.js
@@ -20,22 +20,21 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      
       if ( userAuth ){
         const userRef = await createUserProfileDocument(userAuth);
+
         userRef.onSnapshot(snapshot => {
           this.setState({
             currentUser: {
               id: snapshot.id,
               ...snapshot.data()
             }
-          })
+          }, () => console.log(this.state))
         });
-      } else {
-        this.setState({
-          currentUser: userAuth
-        })
       }
+
+      this.setState({ currentUser: userAuth })
+
     })
   }
 
--- src/pages/sing-in-out/sing-in-out.component.jsx
+++ src/pages/sing-in-out/sing-in-out.component.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './sing-in-out.styles.scss'
 import SingIn from '../../components/sing-in/sing-in.component'
+import SignUp from '../../components/sing-up/sing-up.component'
 
 const SingInOut = () => {
     return (
         <div className="sing-in-and-sing-up">
             <SingIn />
+            <SignUp />
         </div>
     )
 }
