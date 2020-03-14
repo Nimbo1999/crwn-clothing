5136456c989ead54c9441394819f80514e17edc8
Matheus Lopes <matlopes1999@gmail.com>
2020-03-14 20:27:45 -0300

    Adicionando styled-components library, e estilizando alguns componentes com essa biblioteca

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles'
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
+const CustomButton = ({ children, ...props }) => {
     return (
-        <button className={`
-                ${inverted ? 'inverted' : null}
-                ${isGoogleSignIn ? 'google-sign-in' : null}
-                custom-button`
-            }
-            {...otherProps}
-        >
+        <CustomButtonContainer {...props}>
             {children}
-        </button>
+        </CustomButtonContainer>
     )
 }
 
--- src/core/header/header.component.jsx
+++ src/core/header/header.component.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import './header.styles.scss';
 import { ReactComponent as  Logo } from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
@@ -10,31 +8,33 @@ import { auth } from '../../firebase/firebase.utils';
 import { createStructuredSelector } from 'reselect'
 import { selectCartHidden } from '../../redux/cart/cart.selectors'
 import { selectCurrentUser } from '../../redux/user/user.selectors'
+// Styled CSS
+import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer } from './header.styles'
 
 const Header = ({ currentUser, hidden }) => {
 
     return (
-        <div className="header">
-            <Link className="logo-container" to="/">
+        <HeaderContainer>
+            <LogoContainer to="/">
                 <Logo className="logo" />
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to="/shop">
                     SHOP
-                </Link>
-                <Link className="option" to="/contact">
+                </OptionLink>
+                <OptionLink to="/contact">
                     CONTACT
-                </Link>
+                </OptionLink>
                 {
                     currentUser ?
-                    <div className="option att" onClick={() => { auth.signOut()}}>SIGN OUT</div>
+                    <OptionLink as='div' onClick={() => { auth.signOut()}}>SIGN OUT</OptionLink>
                     :
-                    <Link className="option" to="/singin">SING IN</Link>
+                    <OptionLink to="/singin">SING IN</OptionLink>
                 }
                 <CartIcon />
-            </div>
+            </OptionsContainer>
             {hidden ? null : <CartDropdown />}
-        </div>
+        </HeaderContainer>
     )
 }
 
