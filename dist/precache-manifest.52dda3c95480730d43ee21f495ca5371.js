4f5312e55071fad3696d51887b268f57eb71fa79
Matheus Lopes <matlopes199@gmail.com>
2020-06-25 22:54:40 -0300

    Firebase hosting deployed

---
+++ dist/precache-manifest.52dda3c95480730d43ee21f495ca5371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67d1514e5c4cfc5e8253d634884cbc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd07dae13a55ef65e004",
+    "url": "/static/css/main.15989451.chunk.css"
+  },
+  {
+    "revision": "ff664a36d55f815a282c",
+    "url": "/static/js/2.027ebc9a.chunk.js"
+  },
+  {
+    "revision": "cd07dae13a55ef65e004",
+    "url": "/static/js/main.c7e5d2fa.chunk.js"
+  },
+  {
+    "revision": "7b4d32f40c17c6e9f027",
+    "url": "/static/js/runtime-main.22e70d0e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
