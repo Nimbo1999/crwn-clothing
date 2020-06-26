0b3561deca16030979148f89acbd586d0e7c3879
Matheus Lopes <matlopes199@gmail.com>
2020-06-25 22:49:56 -0300

    + Add root-saga, so we can register all sagas codes.

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put } from 'redux-saga/effects'
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 import ShopActionTypes from './shop.types'
 import { fetchCollectionsSuccess, fetchCollectionsError } from './shop.actions'
@@ -18,7 +18,7 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
