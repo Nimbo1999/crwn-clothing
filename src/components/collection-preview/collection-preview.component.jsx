cd8259c53aeb3b501fa6b70ca379fa5077ef2594
Matheus Lopes <matlopes199@gmail.com>
2020-07-09 18:38:41 -0300

    Fix responsive page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { useLocation, Link } from 'react-router-dom';
 
 import './collection-preview.styles.scss'
 import CollectionItem from '../collection-item/collection-item.component'
 
-const CollectionPreview = ( {title, items} ) => (
-    <div className="collection-preview">
-        <h1 className="title">{ title.toUpperCase() }</h1>
-        <div className="preview">
-            {items
-                .filter((item, idx) => idx < 4)
-                .map( (item) => (
-                    <CollectionItem key={item.id} item={item} />
-                ))}
+const CollectionPreview = ( {title, items, routeName} ) => {
+
+    const { pathname } = useLocation();
+
+    return (
+        <div className="collection-preview">
+            <Link to={`${pathname}/${routeName}`} className="title">{ title.toUpperCase() }</Link>
+            <div className="preview">
+                {items
+                    .filter((item, idx) => idx < 4)
+                    .map( (item) => (
+                        <CollectionItem key={item.id} item={item} />
+                    ))}
+            </div>
         </div>
-    </div>
-)
+    );
+}
 
 export default CollectionPreview
